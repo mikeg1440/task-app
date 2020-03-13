cf663ced83e2e98f6fd9f1e9d51104f5f5add33a
Michael Gaudrau <gaudreaum01@gmail.com>
2020-03-13 16:11:13 -0400

    Add tests for creating tasks in task_spec file

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,7 +1,33 @@
 
 require_relative '../spec_helper.rb'
 
-describe 'Creating a task' do
+describe 'Task' do
+
+  user = User.create(username: "tester", email: "tester@mail.com", password: "password", password_confirmation: "password" )
+
+  it 'should be invalid without a title' do
+    task = user.tasks.build(description: 'Take out trash before dinner', due_time: Time.now + 60*60)
+    user.save
+    expect(task).to be_invalid
+  end
+
+  it 'should be valid with a title' do
+    task = user.tasks.build(title: 'Empty trash')
+    user.save
+    expect(task).to be_valid
+  end
+
+  it 'should be valid with a description' do
+    task = user.tasks.build(title: 'Empty trash', description: 'Take out trash before dinner')
+    user.save
+    expect(task).to be_valid
+  end
+
+  it 'should be valid with a due time' do
+    task = user.tasks.build(title: 'Empty trash', description: 'Take out trash before dinner', due_time: Time.now + 60*60)
+    user.save
+    expect(task).to be_valid
+  end
 
 
 end
