a8d1d3cda252925ae524d1a6151f54f356ac36bd
Michael Gaudrau <gaudreaum01@gmail.com>
2020-03-13 14:33:33 -0400

    Add tests to user_spec for creating a user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,35 @@
+
 require_relative '../spec_helper.rb'
 
 describe 'Creating a new user' do
+
+  context 'should allow user to be' do
+
+    it 'valid if created with proper variables' do
+      user = User.create(username: "tester", email: "tester@mail.com", password: "password", password_confirmation: "password" )
+      expect(user).to be_valid
+    end
+
+    it 'invalid if created without a email' do
+      user = User.create(username: "tester", password: "password", password_confirmation: "password" )
+      expect(user).to be_invalid
+    end
+
+    it 'invalid if created without a password' do
+      user = User.create(username: "tester", email: "tester@mail.com")
+      expect(user).to be_invalid
+    end
+
+    it 'invalid if created without a password confirmation' do
+      user = User.create(username: "tester", email: "tester@mail.com", password: 'password')
+      expect(user).to be_invalid
+    end
+
+    it 'invalid if created without a username' do
+      user = User.create(email: "tester@mail.com", password: "password", password_confirmation: "password" )
+      expect(user).to be_invalid
+    end
+
+  end
+
 end
