b1240594c0fd821489c636e8de34082895c22b10
Michael Gaudrau <gaudreaum01@gmail.com>
2020-03-13 16:10:18 -0400

    Add priorities to the seed file tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 end
 
 user1 = User.first
-user1.tasks << Task.create(title: "Create task routes", description: "Make sure all task routes are setup and working")
-user1.tasks << Task.create(title: "Make spec tests for tasks", description: "Create spec tests that check if task routes are working correctly")
-user1.tasks << Task.create(title: "Create user routes", description: "Create the routes for managing users and sessions")
-user1.tasks << Task.create(title: "DRY out CSS files and compress", description: "CSS files are a mess, there are some custom classes that are not used or redundant")
-user1.tasks << Task.create(title: "Add theme customiztion", description: "Allow users to set different colors for the background and fonts etc..")
+user1.tasks << Task.create(title: "Create task routes", description: "Make sure all task routes are setup and working", priority: 'High')
+user1.tasks << Task.create(title: "Make spec tests for tasks", description: "Create spec tests that check if task routes are working correctly", priority: 'Medium')
+user1.tasks << Task.create(title: "Create user routes", description: "Create the routes for managing users and sessions", priority: 'High')
+user1.tasks << Task.create(title: "DRY out CSS files and compress", description: "CSS files are a mess, there are some custom classes that are not used or redundant", priority: 'Low')
+user1.tasks << Task.create(title: "Add theme customiztion", description: "Allow users to set different colors for the background and fonts etc..", priority: 'Medium')
 
 # Add a task to find where a random quote came from for all 5 users
 User.all.each do |user|
